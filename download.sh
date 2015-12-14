a9d914957656d36c02b67f0ea9cf93249c43481f
Gig77 <frech.christian@gmail.com>
2015-12-14 12:03:34 +0100

    none

---
+++ download.sh
@@ -0,0 +1,13 @@
+#wget -c --no-check-certificate --auth-no-challenge --limit-rate=1500k --user 'katarzyna.pietrzykowska@ccri.at' --password 'kasiacsf' http://ngs.csf.ac.at/data/33896_ATCACG_C89VTANXX_1_20151209B_20151209.bam
+#wget -c --no-check-certificate --auth-no-challenge --limit-rate=1500k --user 'katarzyna.pietrzykowska@ccri.at' --password 'kasiacsf' http://ngs.csf.ac.at/data/33897_CGATGT_C89VTANXX_1_20151209B_20151209.bam
+#wget -c --no-check-certificate --auth-no-challenge --limit-rate=1500k --user 'katarzyna.pietrzykowska@ccri.at' --password 'kasiacsf' http://ngs.csf.ac.at/data/33898_TTAGGC_C89VTANXX_1_20151209B_20151209.bam
+wget -c --no-check-certificate --auth-no-challenge --limit-rate=1500k --user 'katarzyna.pietrzykowska@ccri.at' --password 'kasiacsf' http://ngs.csf.ac.at/data/33899_TGACCA_C89VTANXX_1_20151209B_20151209.bam
+wget -c --no-check-certificate --auth-no-challenge --limit-rate=1500k --user 'katarzyna.pietrzykowska@ccri.at' --password 'kasiacsf' http://ngs.csf.ac.at/data/33900_ACAGTG_C89VTANXX_1_20151209B_20151209.bam
+wget -c --no-check-certificate --auth-no-challenge --limit-rate=1500k --user 'katarzyna.pietrzykowska@ccri.at' --password 'kasiacsf' http://ngs.csf.ac.at/data/33901_GCCAAT_C89VTANXX_1_20151209B_20151209.bam
+wget -c --no-check-certificate --auth-no-challenge --limit-rate=1500k --user 'katarzyna.pietrzykowska@ccri.at' --password 'kasiacsf' http://ngs.csf.ac.at/data/33902_CAGATC_C89VTANXX_2_20151209B_20151209.bam
+wget -c --no-check-certificate --auth-no-challenge --limit-rate=1500k --user 'katarzyna.pietrzykowska@ccri.at' --password 'kasiacsf' http://ngs.csf.ac.at/data/33903_CTTGTA_C89VTANXX_2_20151209B_20151209.bam
+wget -c --no-check-certificate --auth-no-challenge --limit-rate=1500k --user 'katarzyna.pietrzykowska@ccri.at' --password 'kasiacsf' http://ngs.csf.ac.at/data/33904_ATCACG_C89VTANXX_2_20151209B_20151209.bam
+wget -c --no-check-certificate --auth-no-challenge --limit-rate=1500k --user 'katarzyna.pietrzykowska@ccri.at' --password 'kasiacsf' http://ngs.csf.ac.at/data/33905_CGATGT_C89VTANXX_2_20151209B_20151209.bam
+wget -c --no-check-certificate --auth-no-challenge --limit-rate=1500k --user 'katarzyna.pietrzykowska@ccri.at' --password 'kasiacsf' http://ngs.csf.ac.at/data/33906_TTAGGC_C89VTANXX_2_20151209B_20151209.bam
+wget -c --no-check-certificate --auth-no-challenge --limit-rate=1500k --user 'katarzyna.pietrzykowska@ccri.at' --password 'kasiacsf' http://ngs.csf.ac.at/data/33907_TGACCA_C89VTANXX_2_20151209B_20151209.bam
+
